479fa88b27da42033e8388847d34a19a9e01eccd
vilmarvalval <ville.valtasalo@metropolia.fi>
2026-01-21 11:27:37 +0200

    express2

--- dev/week1/express1/app.js
+++ dev/week1/express1/app.js
@@ -2,14 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+//Root endpoint callback
 const handleRootRequest = (req, res) => {
     res.send('Hello World!');
 };
 
+//Server listening callback
 const handleServerListening = () => {
     console.log(`Example app listening on port ${port}`);
 };
 
+//register routes with callback
 app.get('/', handleRootRequest);
-
+//Start server
 app.listen(port, handleServerListening);
